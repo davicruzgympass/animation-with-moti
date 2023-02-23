19ce50a3e2795b1f483e4c942bce2f6500264ce8
Davi Cruz <ext.invillia.davi.cruz@gympass.com>
2023-02-23 09:18:29 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import React, { useState } from 'react'
-import { Image, StyleSheet, Text, View, TouchableOpacity, Pressable, Dimensions } from 'react-native'
-import { MotiView, useAnimationState } from 'moti'
+import React, { useState, useRef } from 'react'
+import { Image, StyleSheet, Text, View, TouchableOpacity, Pressable, Dimensions, Animated } from 'react-native'
+import { MotiView, useAnimationState, AnimatePresence } from 'moti'
 
 const {height} = Dimensions.get('screen')
 function ShapeMin() {
   const [isExpanded,setIsExpanded] = useState(false)
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1800,
+      delay: 100,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 1800,
+      useNativeDriver: true,
+    }).start();
+  };
 
   const animationState = useAnimationState({
   from: {
@@ -21,14 +40,16 @@ function ShapeMin() {
 
 const onPress = () => {
   if (animationState.current !== 'expanded') {
-    animationState.transitionTo('expanded')
+    animationState.transitionTo('expanded');
     setTimeout(() => {
-  setIsExpanded(!isExpanded)
-    }, 400)
+      setIsExpanded(!isExpanded)
+      fadeIn();
+    }, 600)
   }else{
     animationState.transitionTo('from')
+    fadeOut()
     setTimeout(() => {
-  setIsExpanded(!isExpanded)
+      setIsExpanded(!isExpanded)
     }, 1600)
   }
 }
@@ -44,36 +65,79 @@ const onPress = () => {
         }
       }
     >
-        
-        <View
-      style={isExpanded ? styles.containerMax : styles.containerMin}
+      <AnimatePresence exitBeforeEnter>
+        <MotiView
+          from={{
+            opacity: 1,
+            scale: 1,
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+          }}
+          exit={{
+            opacity: 0,
+            scale: 0.9,
+          }}
+          transition={{
+            duration: 1800,
+            type: 'timing'
+          }}
         >
 
-          {
-            isExpanded && (<Image style={styles.image} source={{uri: 'https://images.unsplash.com/photo-1677009741202-b761c523fd15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'}} />)
-          }
+        {
+          isExpanded ? (
+           
+              <Animated.View
+              style={[
+                styles.containerMax,
+                {
+                  opacity: fadeAnim,
+                },
+              ]}>
 
-        <Text style={{maxWidth: '50%'}}>[Titulo do audio]</Text>
-
-        <View style={isExpanded ? styles.containerButtonMax : styles.containerButtonMin}>
-
-
-          <TouchableOpacity>
-            <Text>⏪</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={onPress}>
-            <Text>⏸</Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Text>⏩</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={onPress}>
-            <Text>❌</Text>
-          </TouchableOpacity>
-        </View>
-    </View>
-      
-    </MotiView>
+              <Image style={styles.image} source={{uri: 'https://images.unsplash.com/photo-1677009741202-b761c523fd15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'}} />
+            <Text>Titulo Teste</Text>
+            <View style={styles.containerButtonMax}>
+              <TouchableOpacity>
+                <Text>⏪</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={onPress}>
+                <Text>⏸</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>⏩</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={onPress}>
+                <Text>❌</Text>
+              </TouchableOpacity>
+            </View>            
+            </Animated.View>
+          ) : (
+            <View
+              style={styles.containerMin}
+            > 
+              <Text>Titulo Teste</Text>
+              <View style={styles.containerButtonMin}>
+                <TouchableOpacity>
+                  <Text>⏪</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onPress}>
+                  <Text>⏸</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Text>⏩</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onPress}>
+                  <Text>❌</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          )
+        }  
+        </MotiView>
+      </AnimatePresence>
+    </MotiView>        
   )
 }
 
